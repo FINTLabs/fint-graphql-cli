5c887b59bcd132b9dccb55ded9fc2723e4ea3366
Asgeir Nilsen <asgeir@fintlabs.no>
2020-01-21 16:26:36 +0100

    Enable async fetch.

--- generate/graphql/query_resolver_tpl.go
+++ generate/graphql/query_resolver_tpl.go
@@ -9,6 +9,9 @@ import {{resourcePkg .Package}}.{{ .Name }}Resource;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.CompletionStage;
 
 @Component("{{component .Package}}{{.Name}}QueryResolver")
 public class {{ .Name }}QueryResolver implements GraphQLQueryResolver {
@@ -16,17 +19,17 @@ public class {{ .Name }}QueryResolver implements GraphQLQueryResolver {
     @Autowired
     private {{ .Name }}Service service;
 
-    public {{ .Name }}Resource get{{ .Name }}(
+    public CompletionStage<{{ .Name }}Resource> get{{ .Name }}(
 {{- range $i, $ident := .Identifiers }}
             String {{ .Name }},
 {{- end }}
             DataFetchingEnvironment dfe) {
 {{- range $i, $ident := .Identifiers }}
         if (StringUtils.isNotEmpty({{ .Name }})) {
-            return service.get{{ $.Name }}ResourceById("{{ lowerCase .Name }}", {{.Name}}, dfe);
+            return service.get{{ $.Name }}ResourceById("{{ lowerCase .Name }}", {{.Name}}, dfe).toFuture();
         }
 {{- end }}
-        return null;
+        return Mono.<{{ .Name }}Resource>empty().toFuture();
     }
 }
 `
